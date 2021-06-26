f7b03474d8af5137f68a3921f08b5d12f8c6ef4f
spankaj0592 <53137014+spankaj0592@users.noreply.github.com>
2021-06-26 10:10:46 +0530

    Update login.service.ts

--- Raw-Scan-Server/src/app/login/login.service.ts
+++ Raw-Scan-Server/src/app/login/login.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'environments/environment';
-import { BehaviorSubject } from 'rxjs';
 
 
 @Injectable({
